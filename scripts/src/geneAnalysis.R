773c1a6aedc389aeb70be6747f79395d517fe836
jmurga <murgamoreno@gmail.com>
2019-03-13 20:04:56 +0100

    dosTable

--- scripts/src/geneAnalysis.R
+++ scripts/src/geneAnalysis.R
@@ -201,3 +201,120 @@ mktByGene <- function(data=NULL,geneList=NULL,test=NULL,population=NULL,cutoff=0
 	return(output)
 
 }
+
+
+dosByGene <- function(data=NULL,geneList=NULL,population=NULL,cutoff=NULL){
+
+	tmp <- data.frame('id'=character(),'population'=character(),'dos'=integer(),'pvalue'=integer())
+	output <- list()
+	for(iter in 1:length(geneList)){
+		## Subset data by geneda
+		print(iter)
+		subsetGene <- data[data[['id']] == geneList[iter],]
+
+		if(dim(subsetGene)[1] > 1){
+			subsetGene <- subsetGene[1]
+		}
+
+		if(subsetGene$m0 == 0 | subsetGene$mi == 0 | subsetGene$p0 == 0 | subsetGene$pi == 0 | subsetGene$di == 0 | subsetGene$d0 == 0){
+			tmpDf <- data.frame('id'=subsetGene$id,'pop'=population,'dos'=NA,'pvalue'=NA)
+			tmp <- rbind(tmp,tmpDf)
+		}
+		else{
+			# DoS = D(n)/(D(n) + D(s)) - P(n)/(P(n) + P(s))
+			if(is.null(cutoff)){
+				dos <- (subsetGene$di/(subsetGene$di + subsetGene$d0)) - (subsetGene$pi/(subsetGene$pi + subsetGene$p0))
+
+				m <- matrix(c(subsetGene$p0, subsetGene$pi, subsetGene$d0, subsetGene$di),ncol = 2)
+				pvalue <- fisher.test(m)$p.value
+				
+				tmpDf <- data.frame('id'=subsetGene$id,'pop'=population,'dos'=dos,'pvalue'=pvalue)
+				tmp <- rbind(tmp,tmpDf)
+
+			}
+			else{
+
+				pi <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+				p0 <- c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+				f <- seq(0.025,0.975,0.05)
+				mi <- 0; m0 <- 0
+				di <- 0; d0 <- 0
+
+				## Group genes
+				pi <- do.call(rbind,lapply(as.character(subsetGene$DAF0f), function(z) unlist(strsplit(z, split = ";")) %>% as.numeric())) %>% colSums()
+				p0 <- do.call(rbind,lapply(as.character(subsetGene$DAF4f), function(z) unlist(strsplit(z, split = ";")) %>% as.numeric())) %>% colSums()
+				mi <- subsetGene$mi
+				m0 <- subsetGene$m0
+				di <- subsetGene$di
+				d0 <- subsetGene$d0
+
+				## Proper formats
+				daf <- cbind(f, pi, p0); daf <- as.data.frame(daf)
+				names(daf) <- c("daf","Pi","P0")
+				div <- cbind(mi, di, m0, d0); div <- as.data.frame(div)
+				names(div) <- c("mi","Di","m0","D0")
+				## Cleaning slightly deleterious mutation by cutoff
+				Pi <- sum(daf$Pi); P0 <- sum(daf$P0)
+				dafBelowCutoff <- daf[daf$daf <= cutoff, ]
+				dafAboveCutoff <- daf[daf$daf > cutoff, ]
+
+
+				mktTableStandard <- data.frame(Polymorphism = c(sum(daf$P0),sum(daf$Pi)), divergence = c(div$D0, div$Di),row.names = c("Neutral class", "Selected class"))
+											   
+				mktTable <- data.frame(`dafBelowCutoff` = c(sum(dafBelowCutoff$P0),sum(dafBelowCutoff$Pi)), `dafAboveCutoff` = c(sum(dafAboveCutoff$P0),
+					sum(dafAboveCutoff$Pi)), row.names = c("neutralClass", "selectedClass"))
+
+				fNeutral <- mktTable[1, 1]/sum(daf$P0)
+				piNeutralBelowCutoff <- Pi * fNeutral
+				piWd <- mktTable[2, 1] - piNeutralBelowCutoff
+				piNeutral <- round(piNeutralBelowCutoff + mktTable[2,2])
+
+				if(piNeutral == 0){
+					tmpDf <- data.frame('id'=subsetGene$id,'pop'=population,'dos'=NA,'pvalue'=pvalue)
+					tmp <- rbind(tmp,tmpDf)
+				}
+				else{
+					dos <- (subsetGene$d0/(subsetGene$d0 + subsetGene$di)) - (subsetGene$p0/(subsetGene$p0 + piNeutral))
+					m <- matrix(c(subsetGene$p0, subsetGene$pi, subsetGene$d0, subsetGene$di),ncol = 2)
+					pvalue <- fisher.test(m)$p.value
+
+					tmpDf <- data.frame('id'=subsetGene$id,'pop'=population,'dos'=dos,'pvalue'=pvalue)
+					tmp <- rbind(tmp,tmpDf)
+				}
+			}
+		}
+	}
+	## Delete list names to not overwrite current content (saving list positions as populations each gene)
+	tmp <- as.data.table(tmp)
+	# tmp[['pvalue']] <- p.adjust(tmp[['pvalue']],method='fdr')
+	output[['dos']] <- tmp
+
+	allGenes <- matrix(c(dim(tmp %>% na.omit)[1],mean(tmp[['dos']],na.rm=T),sd(tmp[['dos']],na.rm=T)),ncol=3,nrow=1)
+	positive <- matrix(c(dim(tmp[dos>0 & pvalue < 0.05])[1],mean(tmp[dos>0 & pvalue < 0.05,dos],na.rm=T),sd(tmp[dos>0 & pvalue < 0.05,dos],na.rm=T)),ncol=3,nrow=1)
+	negative <- matrix(c(dim(tmp[dos<0 & pvalue < 0.05])[1],mean(tmp[dos<0 & pvalue < 0.05,dos],na.rm=T),sd(tmp[dos<0 & pvalue < 0.05,dos],na.rm=T)),ncol=3,nrow=1)
+
+	# Formating alpha table with total, mean and sd
+	output[['dosTable']] <- as.data.frame(rbind(allGenes,positive,negative))
+
+	colnames(output[['dosTable']]) <- c('N','mean','sd')
+
+	if(is.null(cutoff)){
+		output[['dosTable']][['Pi']] <- 'rawPi'
+		output[['dosTable']][['type']] <- c('allGenes','positive','negative')
+		output[['dosTable']][['pop']] <- population
+
+		output[['dosTable']] <- output[['dosTable']][,c('Pi','type','N','mean','sd','pop')]
+		
+		output[['posSigGenes']] <- as.character(tmp[dos>0 & pvalue < 0.05,id])
+	}else{
+		output[['dosTable']][['Pi']] <- 'PiNeutral'
+		output[['dosTable']][['type']] <- c('allGenes','positive','negative')
+		output[['dosTable']][['pop']] <- population
+
+		output[['dosTable']] <- output[['dosTable']][,c('test','type','N','mean','sd','pop')]
+		
+		output[['posSigGenes']] <- as.character(tmp[alpha>0 & pvalue < 0.05,id])
+	}
+
+	return(output)
+}
