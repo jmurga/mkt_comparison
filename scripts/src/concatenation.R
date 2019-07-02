a6f543dae7414c0a6228c741731ab8d17ee36958
jmurga <jmurga@hotmail.es>
2019-07-02 13:26:06 +0200

    newWdFraction2

--- scripts/src/concatenation.R
+++ scripts/src/concatenation.R
@@ -103,3 +103,91 @@ sampleAnalysis <- function(data,sampling,bins,population,recomb=FALSE,xlow=0.1,x
 	return(output)
 }
 
+wdOnConcatenation <- function(sizes,pop,data){
+
+	out <- list()
+
+	for(s in sizes){
+
+		print(s)
+
+		genes <- unique(data[['id']]) %>% as.matrix()
+		set.seed(13753)
+		subsetGenes <- sample(genes,size=s)
+
+		bootSamplesBin <- samplingGenes(geneList = subsetGenes,B = 3500,bins = s)
+		data = data; sampling = bootSamplesBin;bins = s; population = pop
+
+		data <- data %>% as.data.table
+		data <- data[pop==population]
+		data$id <- as.character(data$id)
+		fractions <- data.table()
+		ci <- data.table()
+		xlow <- 0; xhigh <- 1
+
+		for(i in 1:100){
+			sampling[[i]] <- sampling[[i]] %>% as.data.frame
+			colnames(sampling[[i]]) <- 'id'
+			# subsetData <- data[match(data$Name, sampling[[i]]),] %>% na.omit()
+			subsetData <- merge(sampling[[i]],data,by='id',all.x=T)
+
+			divergence <- subsetData[,c('mi', 'di', 'm0', 'd0')]
+			colnames(divergence) <- c('mi', 'Di', 'm0', 'D0')
+			divergence <- divergence %>% summarize(mi=sum(mi),Di=sum(Di),m0=sum(m0),D0=sum(D0))
+
+			sfs0f <- as.data.frame(subsetData[['DAF0f']])
+			colnames(sfs0f) <- 'sfs0f'
+			sfs0f <- cSplit(sfs0f, 'sfs0f', ';') %>% colSums
+			sfs0f <- as.numeric(sfs0f)
+
+			sfs4f <- as.data.frame(subsetData[['DAF4f']]) 
+			colnames(sfs4f) <- 'sfs4f'
+			sfs4f <- cSplit(sfs4f, 'sfs4f', ';') %>% colSums
+			sfs4f <- as.numeric(sfs4f)
+
+			daf <- cbind(daf=seq(0.05,1,0.05), sfs0f, sfs4f) %>% as.data.frame
+			colnames(daf) <- c('daf','Pi','P0')
+			# daf<-daf[daf$daf <= 0.9, ]
+			D0 <- divergence$D0
+			Di <- divergence$Di
+			Pi <- sum(daf$Pi)
+			P0 <- sum(daf$P0)
+
+			resultsDGRP1 <- DGRP(daf=daf,div=divergence,listCutoffs=0.05)
+			fractions <- rbind(fractions,data.table(resultsDGRP1[['Fractions']][2,],resultsDGRP1[['Fractions']][3,],'eMKT 5%'))
+			resultsDGRP2 <- DGRP(daf=daf,div=divergence,listCutoffs=0.1)
+			fractions <- rbind(fractions,data.table(resultsDGRP2[['Fractions']][2,],resultsDGRP2[['Fractions']][3,],'eMKT 10%'))
+			
+			
+			resultsiMKT <- tryCatch(iMKT(daf=daf,div=divergence,xlow=xlow,xhigh=xhigh,plot=T),error=function(e){resultiMKT=NULL})
+
+			if(is.null(resultsiMKT)){
+				
+				output <- rbind(output,c(NA,NA,'aMKT 5%'))
+				resultsiMKT3 <- c(NA,NA,'aMKT')
+				
+			}
+			else{
+
+				fractions <- rbind(fractions,data.table(resultsiMKT[['Fractions']][2,2],resultsiMKT[['Fractions']][4,2],'aMKT 5%'))
+				fractions <- rbind(fractions,data.table(resultsiMKT[['Fractions']][3,2],resultsiMKT[['Fractions']][5,2],'aMKT'))
+
+				ci <- rbind(ci,data.table(resultsiMKT[['asymptoticMkTable']][['alpha_asymptotic']],resultsiMKT[['asymptoticMkTable']][['CI_low']],resultsiMKT[['asymptoticMkTable']][['CI_high']],as.numeric(s)))
+			}
+		}
+		
+		colnames(fractions) <- c('f','b','test')
+		fractions[['test']] <- factor(fractions[['test']],levels=c('eMKT 5%','eMKT 10%','aMKT 5%','aMKT'))
+	 	
+		df <- melt(fractions)
+
+		p <- ggplot(df) + geom_boxplot(aes(x=test,y=value,fill=variable)) + themePublication() + scaleFillPublication() + xlab('Method') + ylab('Proportion of mutations') + guides(fill=guide_legend(title='Mutation type'))
+
+		out[[paste0(s)]][['plot']] <- p
+		colnames(ci) <- c('alpha','ciLow','ciHigh','subetLength')
+		out[[paste0(s)]][['ci']] <- ci
+
+	}
+
+	return(out)
+}
