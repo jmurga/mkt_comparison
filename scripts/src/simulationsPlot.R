48677cc850b68ca09cdc79c45700cea9bbd563bf
jmurga <jmurga@hotmail.es>
2019-10-11 15:51:56 +0200

    wdTablesAndPlots

--- scripts/src/simulationsPlot.R
+++ scripts/src/simulationsPlot.R
@@ -100,84 +100,67 @@ mktOnsimulatedData <- function(scenario,simulationsPath='/home/jmurga/mkt/201902
 }
 
 wdOnsimulatedData <- function(scenario,simulationsPath){
+
 	output <- NULL
 	plotTable <- list()
 
-	setwd(paste0(simulationsPath,scenario,'/'))
-	listDaf <- lapply(list.files(pattern = 'daf',recursive=T),fread)
-	listDiv <- lapply(list.files(pattern = 'div',recursive=T),fread,sep='\t',header=T)
+	setwd(paste0(simulationsPath,'/',scenario,'/'))
+	# setwd(paste0('/home/jmurga/mkt/201902/rawData/simulations/alphaComparisons/l1e6'))
+	listDaf <- lapply(list.files(pattern = 'daf',recursive=T),read.table,sep='\t',header=T)
+	listDiv <- lapply(list.files(pattern = 'div',recursive=T),read.table,sep='\t',header=T)
 	
 	for(simulation in 1:length(listDaf)){
+	# for(simulation in 1:10){
 
 		print(simulation)
 
 		daf <- listDaf[[simulation]][,c('daf','Pi','P0')]
 		divergence <- listDiv[[simulation]][,c('Di','D0','m0','mi')]
-		trueAlpha <- listDiv[[simulation]]$trueAlpha
-		trueB <- listDiv[[simulation]]$b
+		trueAlpha <- listDiv[[simulation]][,c('trueAlpha')]
+		trueB <- listDiv[[simulation]][,c('b')]
 
-		# Execute each mkt test through iMKT package
 		##DGRP
-		resultDGRP <- eMKT(daf,divergence, plot=F)
-		alphaDGRP <- resultDGRP$alphaCorrected$alphaCorrected
+		resultDGRP <- eMKT(daf,divergence, listCutoffs=c(0.025,0.075), plot=F)
 		bDGRP <- resultDGRP$fractions$b
 
 		##Asymptotic
-		resultiMK1 <- tryCatch({
-			aMKT(daf,divergence,xlow = 0, xhigh = 1, plot=F)
-			},error=function(e){
-				return(NULL)
+		bAsymptotic1 <- tryCatch({
+			resultiMK1 <- aMKT(daf,divergence,xlow = 0, xhigh = 1, plot=F)
+			bAsymptotic1 <- resultiMK1$fractions$b
+			},
+			error=function(e)
+			{
+				bAsymptotic1 <- NA
 			}
 		)
 
-		resultiMK2 <- tryCatch({
-			aMKT(daf,divergence,xlow = 0.1, xhigh = 0.9, plot=F)
-			},error=function(e){ 
-				return(NULL)
+		bAsymptotic2 <- tryCatch({
+			resultiMK2 <- aMKT(daf,divergence,xlow = 0.1, xhigh = 0.9, plot=F)
+			bAsymptotic2<- resultiMK2$fractions$b
+			},
+			error=function(e){ 
+				bAsymptotic2 <- NA
 			}
 		)
 
-		if(is.null(resultiMK1))
-		{
-			alphaAsymptotic1 <- NA
-			alphaAsymptotic2 <- NA
-			bAsymptotic1 <- NA
-			bAsymptotic2 <- NA
-		}
-		else if(is.null(resultiMK2))
-		{
-			alphaAsymptotic1 <- resultiMK1$alphaCorrected$alphaAsymptotic
-			bAsymptotic1 <- resultiMK1$Fractions[3,1]
-			alphaAsymptotic2 <- NA
-			bAsymptotic2 <- NA
-		}
-		else
-		{
-			alphaAsymptotic1 <- resultiMK1$alphaCorrected$alphaAsymptotic
-			bAsymptotic1 <- resultiMK1$Fractions[3,1]
-			alphaAsymptotic2 <- resultiMK2$alphaCorrected$alphaAsymptotic
-			bAsymptotic2 <- resultiMK2$Fractions[3,1]
-		}
-
-		resultAlpha <- data.table('DGRP'=alphaDGRP,'asymp1'=alphaAsymptotic1,'asymp2'=alphaAsymptotic2,'trueValue'=trueAlpha,'measure'='alpha')
-		resultB <- data.table('DGRP'=bDGRP,'asymp1'=bAsymptotic1,'asymp2'=bAsymptotic2,'trueValue'=trueB,'measure'='b')
-
-		result <- rbind(resultAlpha,resultB)
+		result <- data.table(simulation,bDGRP[1],bDGRP[2],bAsymptotic1,bAsymptotic2,trueB)
+		colnames(result) <- c('simulation','bDGRP1','bDGRP2','bAsymptotic1','bAsymptotic2','trueB')
 		output <- rbind(output,result)
 	}
 	
 	# Preparing data to plot
-	dataPlot <- reshape2::melt(output,id=c('measure'))
+	dataPlot <- reshape2::melt(output,id=c('simulation'))
 
 	# Errors
-	errorB <- output[output$measure == 'b',]
-	DGRPError <- mean(abs(errorB[['DGRP']]-errorB[['trueValue']]),na.rm=TRUE)
-	asymptotic1Error <- mean(abs(errorB[['asymp1']]-errorB[['trueValue']]),na.rm=TRUE)
-	asymptotic2Error <- mean(abs(errorB[['asymp2']]-errorB[['trueValue']]),na.rm=TRUE)
+	DGRPError <- mean(abs(output[['bDGRP1']]-output[['trueB']]),na.rm=TRUE)
+	DGRP2Error <- mean(abs(output[['bDGRP2']]-output[['trueB']]),na.rm=TRUE)
+	asymptoticError <- mean(abs(output[['bAsymptotic1']]-output[['trueB']]),na.rm=TRUE)
+	asymptotic1Error <- mean(abs(output[['bAsymptotic2']]-output[['trueB']]),na.rm=TRUE)
 
-	error <- matrix(c(DGRPError,asymptotic1Error,asymptotic2Error,0),nrow=1,ncol=4)
+	error <- matrix(c(DGRPError,DGRP2Error,asymptoticError,asymptotic1Error,0),nrow=1,ncol=5)
 
-	meanSd <- sapply(errorB[,1:4], function(cl) list(means=mean(cl,na.rm=TRUE), sds=sd(cl,na.rm=TRUE)))
+	meanSd <- sapply(output, function(cl) list(means=mean(cl,na.rm=TRUE), sds=sd(cl,na.rm=TRUE)))
+	meanSd <- meanSd[,-1]
 	meanSd <- rbind(meanSd,error)
 	meanSd <- as.data.frame(meanSd)
 	rownames(meanSd) <- c('mean','sd','error')
@@ -185,23 +168,24 @@ wdOnsimulatedData <- function(scenario,simulationsPath){
 	meanSd$scenario <- scenario
 
 	# Ploting
-	dp <- dataPlot[dataPlot$measure=='b',]
-	p <- ggplot(dp, aes(x=variable, y=value, fill=measure)) + 
-		geom_boxplot(color="grey20",alpha=0.7,show.legend=F) + 
-		labs(x = "MKT methods",y="B estimations") + 
+	plotAlpha <- ggplot(dataPlot, aes(x=variable, y=value, fill=variable)) + 
+		geom_boxplot(color="grey20",alpha=0.7) + 
+		labs(x = "MKT methods", y=expression(italic(α))) + 
 		themePublication() + 
-		scaleFillPublication(name="Method", labels=c("DGRP" = "eMKT","asymp1"="Asymptotic MKT","asymp2"="Asymptotic MKT 0.1-0.9", "trueValue"="True alpha/b")) + 
+		scaleFillPublication(name="Method", labels=c('bDGRP1' = 'eMKT 5%', 'bDGRP2' = 'eMKT 10%', 'bFWW1' = 'FWW 5%', 'bFWW2' = 'FWW 10%','bAsymptotic1'='Asymptotic MKT','bAsymptotic2'='Asymptotic MKT', 'trueB'='True b')) + 
 		scale_y_continuous(breaks = pretty(dataPlot$value, n = 5)) + 
-		scale_x_discrete(labels=c("DGRP" = "eMKT","asymp1"="Asymptotic MKT","asymp2"="Asymptotic MKT 0.1-0.9", "trueValue"="b")) +  
+		scale_x_discrete(labels=c('bStandard'='Standard', 'bDGRP1' = 'eMKT 5%', 'bDGRP2' = 'eMKT 10%','bAsymptotic1'='Asymptotic MKT','bAsymptotic2'='Asymptotic MKT 0.1-0.9', 'trueB'='True b')) +  
+		guides(fill=FALSE) + 
 		ggtitle(paste0(scenario)) + 
 		theme(axis.text.x = element_text(angle = 45, hjust = 1,size=20),axis.text.y= element_text(size=20),plot.title=element_text(size=20),axis.title.y = element_text(size=24),axis.title.x = element_text(size=24))
 
-	method <- rep(c("Standard","eMKT","FWW cutoff 5%","MKT FWW cutoff 10%","Asymptotic MKT","Asymptotic MKT 0.1-0.9","True alpha"),2)
+	# method <- rep(c("Standard","eMKT","FWW cutoff 5%","MKT FWW cutoff 10%","Asymptotic MKT","Asymptotic MKT 0.1-0.9","True alpha"),2)
 					 
 
-	plotTable[['plot']] <- p
+	plotTable[['plot']] <- plotAlpha
 	plotTable[['table']] <- meanSd
 	plotTable[['data']] <- dataPlot
 					 
 	return(plotTable)
+					 
 }
